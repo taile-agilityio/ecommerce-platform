7b8b1f8dd5dcccd6cab099c9ad7fcd7a2adea519
Luan Vu <luan.vu@asnet.com.vn>
2025-12-11 14:54:56 +0700

    chore: install and config storybook and tailwindcss

--- packages/ui/eslint.config.mjs
+++ packages/ui/eslint.config.mjs
@@ -1,6 +1,18 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
 
 import { config } from "@repo/eslint-config/react-internal";
 
 /** @type {import("eslint").Linter.Config} */
-export default [...config];
+export default [
+  ...config,
+  {
+    files: ["**/*.stories.@(js|jsx|ts|tsx)", "*.story.@(js|jsx|ts|tsx)"],
+    plugins: {
+      storybook,
+    },
+    rules: {
+      ...storybook.configs.recommended.rules,
+    },
+  },
+];
